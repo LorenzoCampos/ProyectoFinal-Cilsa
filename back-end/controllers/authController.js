492c50251c10167f1fc7fe56c03ce0c2eb0e53fa
LoloCampos <lolocampos544@gmail.com>
2024-11-25 14:32:52 -0300

    Validation implementation.

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -3,13 +3,31 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 const Token = require("../models/tokenModel");
 
+// Expresión regular para validar la contraseña (al menos un número, 8 caracteres, sin espacios)
+const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])(?!.*\s).{8,}$/;
+
+// Expresión regular para validar el username (mínimo 8 caracteres, sin espacios)
+const usernameRegex = /^[^\s]{8,}$/;
+
 // Registro
 const register = async (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password)
-    return res
-      .status(400)
-      .json({ error: "Username y password son requeridos" });
+
+  // Validación de username
+  if (!username || !usernameRegex.test(username)) {
+    return res.status(400).json({
+      error:
+        "El nombre de usuario debe tener al menos 8 caracteres y no puede contener espacios.",
+    });
+  }
+
+  // Validación de password
+  if (!password || !passwordRegex.test(password)) {
+    return res.status(400).json({
+      error:
+        "La contraseña debe tener al menos 8 caracteres, contener al menos un número y no puede contener espacios.",
+    });
+  }
 
   const hashedPassword = await bcrypt.hash(password, 10);
   try {
@@ -27,6 +45,23 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ where: { username } });
+
+  // Validación de username
+  if (!username || !usernameRegex.test(username)) {
+    return res.status(400).json({
+      error:
+        "El nombre de usuario debe tener al menos 8 caracteres y no puede contener espacios.",
+    });
+  }
+
+  // Validación de password
+  if (!password || !passwordRegex.test(password)) {
+    return res.status(400).json({
+      error:
+        "La contraseña debe tener al menos 8 caracteres, contener al menos un número y no puede contener espacios.",
+    });
+  }
+
   if (!user || !(await bcrypt.compare(password, user.password)))
     return res.status(401).json({ error: "Credenciales inválidas" });
 
