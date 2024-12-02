e850d480a02199c51dc35b73a6a6c73a71d34536
LoloCampos <lolocampos544@gmail.com>
2024-12-02 00:30:30 -0300

    Added logout.

--- front-end/src/Navbar.jsx
+++ front-end/src/Navbar.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
 import { Link } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import apiClient from './api/apiClient';
 
 const Navbar = () => {
   // Obtener la fecha actual
   const today = new Date();
-  const formattedDate = today.toLocaleDateString('es-ES', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+  const formattedDate = today.toLocaleDateString("es-ES", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
+
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("authToken");
+
+  // Manejar cerrar sesión
+  const handleLogout = async () => {
+    try {
+      await apiClient.post('/logout');
+      localStorage.removeItem('authToken');
+      navigate('/login');
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+    }
+  };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light ">
@@ -26,35 +43,26 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Inicio
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                Usuario
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contacto
-              </Link>
-            </li>
-          </ul>
-        </div>
-
         <div className="d-flex justify-content-end align-items-center">
-          <span className="navbar-text">
+          <span className="navbar-text me-3">
             {formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)}
           </span>
+        </div>
+
+        <div className="d-flex justify-content-start align-items-center">
+          {token ? (
+            <button className="btn btn-outline-danger" onClick={handleLogout}>
+              Cerrar sesión
+            </button>
+          ) : (
+            <Link className="btn btn-outline-primary" to="/login">
+              Iniciar sesión
+            </Link>
+          )}
         </div>
       </div>
     </nav>
   );
 };
-
 
 export default Navbar;
