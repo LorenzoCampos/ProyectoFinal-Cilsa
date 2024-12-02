b73da71a5ffef1aeef4eb93c1f328c839432837d
LoloCampos <lolocampos544@gmail.com>
2024-12-02 00:28:45 -0300

    Added routing management.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
 import TaskManager from "./TaskManager"; // Administrador de tareas
 import Register from "./Register"; // Componente de registro
 import Login from "./Login";
 
 const App = () => {
+  const token = localStorage.getItem("authToken");
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TaskManager />} /> {/* Página principal */}
-        <Route path="/register" element={<Register />} /> {/* Registro */}
-        <Route path="/login" element={<Login />} /> {/* Login */}
+        <Route path="/" element={token ? <TaskManager /> : <Navigate to="/login" replace />} />
+        <Route path="/register" element={token ? <Navigate to="/" replace /> : <Register />} />
+        <Route path="/login" element={token ? <Navigate to="/" replace /> : <Login />} />
       </Routes>
     </Router>
   );
